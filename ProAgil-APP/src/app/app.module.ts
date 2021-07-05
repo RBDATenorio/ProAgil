0ee0aa26290b496bb48afec6b1df18b42d38e189
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-07-04 22:11:25 -0300

    adding ngx-bootstrap

--- ProAgil-APP/src/app/app.module.ts
+++ ProAgil-APP/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +21,9 @@ import { DateTimeFormatPipe } from './_helper/dateTimeFormat.pipe';
   ],
   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule
