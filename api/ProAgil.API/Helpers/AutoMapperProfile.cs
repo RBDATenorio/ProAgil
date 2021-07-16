94b438bfa4e6dfcbc46c37ac397d1527c384bdb3
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-07-16 09:58:13 -0300

    adding many to many with dto

--- api/ProAgil.API/Helpers/AutoMapperProfile.cs
+++ api/ProAgil.API/Helpers/AutoMapperProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using ProAgil.API.DTOs;
 using ProAgil.Domain.Entities;
@@ -8,8 +9,14 @@ namespace ProAgil.API.Helpers
     {
         public AutoMapperProfile()
         {
-            CreateMap<Evento, EventoDTO>();
-            CreateMap<Palestrante, PalestranteDTO>();
+            CreateMap<Evento, EventoDTO>()
+                        .ForMember(dest => dest.Palestrantes, opt => {
+                            opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Palestrante).ToList());
+                        });
+            CreateMap<Palestrante, PalestranteDTO>()
+                        .ForMember( dest =>  dest.Eventos, opt => {
+                            opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Evento).ToList());
+                        });
             CreateMap<RedeSocial, RedeSocialDTO>();
             CreateMap<Lote, LoteDTO>();
 
